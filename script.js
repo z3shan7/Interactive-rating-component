81ed7cef5183a946fe9b4c2efaa29218ad985986
Shoaib Zeshan <szeeshan707@gmail.com>
2022-09-07 19:16:01 +0530

    on focus added to button

--- script.js
+++ script.js
@@ -5,28 +5,37 @@ const thankYouState = document.querySelector('.thank-you-state');
 const rated = document.getElementById('rated');
 
 
-subBtn.disabled = true 
+subBtn.disabled = true
 
-allBtn.forEach((btn, i )=>{
-    btn.addEventListener('click',()=>{
-        let ratingCount = i + 1 ;
+
+
+
+
+allBtn.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        let ratingCount = i + 1;
         rated.innerHTML = ` You selected ${ratingCount} out of 5`
-        btn.style.backgroundColor = 'hsl(216, 12%, 54%)'
-        btn.style.color = '#fff'
-        if(ratingCount = 1){
-            subBtn.disabled = false 
+
+
+        btn.addEventListener('keyup', function () {
+
+
+        })
+
+
+        if (ratingCount = 1) {
+            subBtn.disabled = false
+
         }
-        else{
-            throw Error('not working')
-        }
+
     })
 
 });
 
-function cssValue(){
+function cssValue() {
     ratingstate.style.display = 'none'
     thankYouState.style.display = 'block'
 }
-subBtn.addEventListener('click',()=>{
+subBtn.addEventListener('click', () => {
     cssValue()
 })
